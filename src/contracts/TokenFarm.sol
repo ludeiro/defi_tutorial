d1618e6087430ecb937eca6597f2c7d626805384
ludeiro <arludeiro@gmail.com>
2021-01-28 16:04:00 +0100

    Part 2_1

---
+++ src/contracts/TokenFarm.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
+contract TokenFarm {
+	
+	string public name = "Dapp TokenFarm";
+
+	DappToken public dappToken;
+	DaiToken public daiToken;
+
+	constructor(DappToken _dappToken, DaiToken _daiToken) public{
+		dappToken = _dappToken;
+		daiToken = _daiToken;
+	}
+
+
+	
+}
