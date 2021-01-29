43f20b19208f4893607de31638d9669d80bd15f6
ludeiro <arludeiro@gmail.com>
2021-01-29 14:12:33 +0100

    issue-tokens script

---
+++ scripts/issue-token.js
@@ -0,0 +1,12 @@
+const TokenFarm = artifacts.require('TokenFarm');
+
+
+module.exports = async function(callback) {
+  	
+  	let tokenFarm = await TokenFarm.deployed()
+  	await tokenFarm.issueTokens()
+  	
+  	console.log("Tokens issued")
+
+	callback()
+}
